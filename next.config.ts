cdec85b577eb40c74f629bc11c599f116b9a5c87
Nhunguy <munnguy@gmail.com>
2026-09-21 13:12:33 +0700

    chore: update project configuration and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["192.168.1.28"],
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "firebasestorage.googleapis.com" },
+    ],
+  },
 };
 
 export default nextConfig;
